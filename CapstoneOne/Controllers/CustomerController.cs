f29e2c34f347fe5030aefa668e492321dd31123c
gracekogithub <graceko02011972@gmail.com>
2021-04-09 15:00:38 -0700

    finished customer's create, index, edit, delete in the customer controller along with views

--- CapstoneOne/Controllers/CustomerController.cs
+++ CapstoneOne/Controllers/CustomerController.cs
@@ -20,11 +20,11 @@ namespace CapstoneOne.Controllers
             _context = context;
         }
         // GET: CustomerController
-        public ActionResult Index()
+        public IActionResult Index()
         {
             var userId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
-            var customer = _context.Customers.Where(c => c.IdentityUserId == userId).SingleOrDefault();
-            if (customer == null)
+            var customer = _context.Customers.Where(c => c.IdentityUserId == userId).ToList();
+            if (customer.Count == 0)
             {
                 return RedirectToAction(nameof(Create));
             }
@@ -33,14 +33,14 @@ namespace CapstoneOne.Controllers
         }
 
         // GET: CustomerController/Details/5
-        public ActionResult Details(int id)
+        public IActionResult Details(int id)
         {
-            var customer = _context.Customers.Find(id);
+            var customer = _context.Customers.Where(e => e.CustomerId == id).FirstOrDefault();
             return View(customer);
         }
 
         // GET: CustomerController/Create
-        public ActionResult Create()
+        public IActionResult Create()
         {
             return View();
         }
@@ -48,7 +48,7 @@ namespace CapstoneOne.Controllers
         // POST: CustomerController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(Customer customer)
+        public IActionResult Create(Customer customer)
         {
             try
             {
@@ -68,14 +68,14 @@ namespace CapstoneOne.Controllers
         // GET: CustomerController/Edit/5
         public ActionResult Edit(int id)
         {
-            var editCustomer = _context.Customers.Find(id);
-            return View(editCustomer);
+            var customer = _context.Customers.Where(e => e.CustomerId == id).FirstOrDefault();
+            return View(customer);
         }
 
         // POST: CustomerController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, Models.Customer customer)
+        public IActionResult Edit(int id, Models.Customer customer)
         {
             try
             {
@@ -83,37 +83,37 @@ namespace CapstoneOne.Controllers
                 customer.IdentityUserId = userId;
                 _context.Customers.Update(customer);
                 _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
             }
             catch
             {
-                Console.WriteLine("Error");
                 return View();
             }
-            return RedirectToAction(nameof(Index));
         }
 
 
         // GET: CustomerController/Delete/5
         public ActionResult Delete(int id)
         {
-            var deleteCustomer = _context.Customers.Find(id);
-            return View(deleteCustomer);
+            var customer = _context.Customers.Where(e => e.CustomerId == id).FirstOrDefault();
+            return View(customer);
         }
 
         // POST: CustomerController/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, Customer customer)
+        public IActionResult Delete(int id, Customer customer)
         {
             try
             {
+                var userId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+                customer.IdentityUserId = userId;
                 _context.Customers.Remove(customer);
                 _context.SaveChanges();
                 return RedirectToAction(nameof(Index));
             }
             catch
             {
-                Console.WriteLine("Error");
                 return View();
             }
         }

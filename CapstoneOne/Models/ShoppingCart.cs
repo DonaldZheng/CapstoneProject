2820087c8e7d856b39dd0cb57241b81ed7614cc0
DonaldZheng <dzheng112@gmail.com>
2021-04-13 09:31:29 -0500

    pushing so ford can pull most recent

---
+++ CapstoneOne/Models/ShoppingCart.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CapstoneOne.Models
+{
+    public class ShoppingCart
+    {
+        public string ShoppingCartId
+        {
+            get; set;
+        }
+        [ForeignKey("Product")]
+        public string ProductId
+        {
+            get; set;
+        }
+        public Product product
+        {
+            get; set; 
+        }
+    }
+}
